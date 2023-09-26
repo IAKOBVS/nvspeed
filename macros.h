b98e0c26878608cbc725aecb4259c3c0c9454b8b
James Tirta Halim <tirtajames45@gmail.com>
2023-09-26 23:07:11 +0700

    add macros.h

---
+++ macros.h
@@ -0,0 +1,20 @@
+#ifndef NVSPEED_MACROS_H
+#define NVSPEED_MACROS_H
+
+#if defined(__GNUC__) || defined(__clang__)
+#	define INLINE __attribute__((always_inline)) inline
+#elif defined(_MSC_VER)
+#	define INLINE __forceinline inline
+#else
+#	define INLINE inline
+#endif
+
+#if (defined(__GNUC__) && (__GNUC__ >= 3)) || (defined(__clang__) && __has_builtin(__builtin_expect))
+#	define likely(x)   __builtin_expect((x), 1)
+#	define unlikely(x) __builtin_expect((x), 0)
+#else
+#	define likely(x)   (x)
+#	define unlikely(x) (x)
+#endif
+
+#endif /* NVSPEED_MACROS_H */
