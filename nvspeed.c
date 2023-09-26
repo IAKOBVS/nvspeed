1c0371585c5075e8de2b9b776c42f14bfbe00a91
James Tirta Halim <tirtajames45@gmail.com>
2023-09-26 23:06:43 +0700

    add macros.h

--- nvspeed.c
+++ nvspeed.c
@@ -9,21 +9,7 @@
 #	include <unistd.h>
 #endif
 
-#if defined(__GNUC__) || defined(__clang__)
-#	define INLINE __attribute__((always_inline)) inline
-#elif defined(_MSC_VER)
-#	define INLINE __forceinline inline
-#else
-#	define INLINE inline
-#endif
-
-#if (defined(__GNUC__) && (__GNUC__ >= 3)) || (defined(__clang__) && __has_builtin(__builtin_expect))
-#	define likely(x)   __builtin_expect((x), 1)
-#	define unlikely(x) __builtin_expect((x), 0)
-#else
-#	define likely(x)   (x)
-#	define unlikely(x) (x)
-#endif
+#include "macros.h"
 
 #ifdef __USE_POSIX199309
 #	define HAVE_NANOSLEEP 1
