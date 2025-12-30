c4a7ac31177d4ed528f05cc0aaa8d59fee28ad7d
James Tirta Halim <tirtajames45@gmail.com>
2025-12-30 17:39:41 +0700

    new

--- nvspeed.c
+++ nvspeed.c
@@ -7,8 +7,43 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include "macros.h"
+
+#define ERR(x)                  \
+	do {                    \
+		xnvml_die(ret); \
+		x;              \
+		assert(0);      \
+	} while (0)
+
+int init = 0;
+nvmlDevice_t *dev;
+int *tmp_temp;
+int *temp;
+
+static void
+xnvml_cleanup()
+{
+	if (init) {
+		free(dev);
+		free(tmp_temp);
+		free(temp);
+		dev = NULL;
+		tmp_temp = NULL;
+		temp = NULL;
+		nvmlShutdown();
+	}
+}
+
+static void
+xnvml_die(nvmlReturn_t ret)
+{
+	perror("\n");
+	fprintf(stderr, "%s\n\n", nvmlErrorString(ret));
+	xnvml_cleanup();
+}
 
 INLINE
 static int
@@ -16,62 +51,46 @@ loop(void)
 {
 	nvmlReturn_t ret = nvmlInit();
 	if (ret != NVML_SUCCESS)
-		goto exit;
+		return EXIT_FAILURE;
+	init = 1;
 	unsigned int num_dev;
 	ret = nvmlDeviceGetCount(&num_dev);
 	if (ret != NVML_SUCCESS)
-		goto shutdown;
-	nvmlDevice_t *const dev = malloc(num_dev * sizeof(nvmlDevice_t));
+		xnvml_die(ret);
+	dev = malloc(num_dev * sizeof(nvmlDevice_t));
 	if (dev == NULL)
-		goto shutdown;
-	int *const tmp = malloc(num_dev * sizeof(int));
-	if (tmp == NULL)
-		goto shutdown_free_dev;
-	memset(tmp, MIN_TEMP, num_dev * sizeof(int));
-	int *const temp = malloc(num_dev * sizeof(int));
+		xnvml_die(ret);
+	tmp_temp = malloc(num_dev * sizeof(int));
+	if (tmp_temp == NULL)
+		xnvml_die(ret);
+	memset(tmp_temp, MIN_TEMP, num_dev * sizeof(int));
+	temp = malloc(num_dev * sizeof(unsigned int));
 	if (temp == NULL)
-		goto shutdown_free_tmp;
+		xnvml_die(ret);
 	for (unsigned int i = 0; i < num_dev; ++i)
 		if ((ret = nvmlDeviceGetHandleByIndex(i, dev + i)) != NVML_SUCCESS)
-			goto cleanup;
+			xnvml_die(ret);
 	unsigned int speed;
 	for (;;) {
 		for (unsigned int i = 0; i < num_dev; ++i) {
 			ret = nvmlDeviceGetTemperature(dev[i], NVML_TEMPERATURE_GPU, (unsigned int *)temp + i);
 			if (unlikely(ret != NVML_SUCCESS))
-				goto cleanup;
-			if (abs(tmp[i] - temp[i]) > MIN_TEMP_DIFF) {
+				xnvml_die(ret);
+			if (abs(tmp_temp[i] - temp[i]) > MIN_TEMP_DIFF) {
 				switch (temp[i]) {
 					CASE_TEMP_SPEED(speed);
 				}
 				ret = nvmlDeviceSetFanSpeed_v2(dev[i], i, speed);
 				if (unlikely(ret != NVML_SUCCESS))
-					goto cleanup;
-				tmp[i] = temp[i];
+					xnvml_die(ret);
+				tmp_temp[i] = temp[i];
 			}
 		}
 		if (unlikely(sleep(1)))
-			goto cleanup;
+			xnvml_die(ret);
 	}
-	free(dev);
-	free(tmp);
-	free(temp);
-	ret = nvmlShutdown();
-	if (ret != NVML_SUCCESS)
-		goto exit;
+	xnvml_cleanup();
 	return EXIT_SUCCESS;
-cleanup:
-	free(temp);
-shutdown_free_tmp:
-	free(tmp);
-shutdown_free_dev:
-	free(dev);
-shutdown:
-	fputs(nvmlErrorString(ret), stderr);
-	nvmlShutdown();
-exit:
-	perror("");
-	return EXIT_FAILURE;
 }
 
 int
