84fa0497b26d589c035d6233b257757ff95f0687
James Tirta Halim <tirtajames45@gmail.com>
2026-01-24 03:22:13 +0700

    nvspeed: optimize

--- nvspeed.c
+++ nvspeed.c
@@ -31,30 +31,40 @@
 
 #include "macros.h"
 
+#ifdef DEBUG
+#	define D_PRINTF(fmt, x) \
+		printf(fmt, x)
+#else
+#	define D_PRINTF(fmt, x)
+#endif
+
 #define ERR(nv_ret)             \
 	do {                    \
 		nv_die(nv_ret); \
 		assert(0);      \
 	} while (0)
 
-int nv_inited;
+/* Global variables */
 nvmlDevice_t *nv_device;
 int *nv_temp_last;
 int *nv_temp;
-unsigned int nv_device_num;
+unsigned int nv_device_count;
+int nv_inited;
+nvmlReturn_t nv_ret;
+int nv_last_speed;
 
 static void
 nv_cleanup()
 {
 	if (nv_inited) {
 		nvmlShutdown();
-		free(nv_device);
-		free(nv_temp_last);
-		free(nv_temp);
-		nv_device = NULL;
-		nv_temp_last = NULL;
-		nv_temp = NULL;
 	}
+	free(nv_device);
+	free(nv_temp_last);
+	free(nv_temp);
+	nv_device = NULL;
+	nv_temp_last = NULL;
+	nv_temp = NULL;
 }
 
 static void
@@ -68,54 +78,58 @@ nv_die(nvmlReturn_t nv_ret)
 static int
 nv_init()
 {
-	nvmlReturn_t ret = nvmlInit();
-	if (ret != NVML_SUCCESS)
-		return -1;
+	nvmlReturn_t nv_ret = nvmlInit();
+	if (nv_ret != NVML_SUCCESS)
+		ERR(nv_ret);
 	nv_inited = 1;
-	unsigned int device_num;
-	ret = nvmlDeviceGetCount(&device_num);
-	if (ret != NVML_SUCCESS)
-		ERR(ret);
-	nv_device = malloc(device_num * sizeof(nvmlDevice_t));
+	nv_ret = nvmlDeviceGetCount(&nv_device_count);
+	if (nv_ret != NVML_SUCCESS)
+		ERR(nv_ret);
+	nv_device = malloc(nv_device_count * sizeof(nvmlDevice_t));
 	if (nv_device == NULL)
-		ERR(ret);
-	nv_temp_last = malloc(device_num * sizeof(int));
+		ERR(nv_ret);
+	nv_temp_last = calloc(nv_device_count, sizeof(int));
 	if (nv_temp_last == NULL)
-		ERR(ret);
-	memset(nv_temp_last, MIN_TEMP, device_num * sizeof(int));
-	nv_temp = malloc(device_num * sizeof(int));
+		ERR(nv_ret);
+	memset(nv_temp_last, MIN_TEMP, nv_device_count * sizeof(int));
+	nv_temp = malloc(nv_device_count * sizeof(int));
 	if (nv_temp == NULL)
-		ERR(ret);
-	for (unsigned int i = 0; i < nv_device_num; ++i)
-		if ((ret = nvmlDeviceGetHandleByIndex(i, nv_device + i)) != NVML_SUCCESS)
-			ERR(ret);
+		ERR(nv_ret);
+	for (unsigned int i = 0; i < nv_device_count; ++i) {
+		nv_ret = nvmlDeviceGetHandleByIndex(i, nv_device + i);
+		if (nv_ret != NVML_SUCCESS)
+			ERR(nv_ret);
+	}
 	return 0;
 }
 
 static int
-nv_loop(void)
+nv_mainloop(void)
 {
 	if (!nv_inited)
 		nv_init();
-	nvmlReturn_t ret = 0;
-	unsigned int speed;
+	int speed;
 	for (;;) {
-		for (unsigned int i = 0; i < nv_device_num; ++i) {
-			ret = nvmlDeviceGetTemperature(nv_device[i], NVML_TEMPERATURE_GPU, (unsigned int *)nv_temp + i);
-			if (unlikely(ret != NVML_SUCCESS))
-				ERR(ret);
+		for (unsigned int i = 0; i < nv_device_count; ++i) {
+			nv_ret = nvmlDeviceGetTemperature(nv_device[i], NVML_TEMPERATURE_GPU, (unsigned int *)nv_temp + i);
+			if (unlikely(nv_ret != NVML_SUCCESS))
+				ERR(nv_ret);
+			D_PRINTF("temp:%d\n", nv_temp[i]);
 			if (abs(nv_temp_last[i] - nv_temp[i]) > MIN_TEMP_DIFF) {
 				switch (nv_temp[i]) {
 					CASE_TEMP_SPEED(speed);
 				}
-				ret = nvmlDeviceSetFanSpeed_v2(nv_device[i], i, speed);
-				if (unlikely(ret != NVML_SUCCESS))
-					ERR(ret);
+				D_PRINTF("speed:%d\n", speed);
+				if (abs(speed - nv_last_speed) > MIN_SPEED_DIFF) {
+					nv_ret = nvmlDeviceSetFanSpeed_v2(nv_device[i], i, (unsigned int)speed);
+					if (unlikely(nv_ret != NVML_SUCCESS))
+						ERR(nv_ret);
+				}
 				nv_temp_last[i] = nv_temp[i];
 			}
 		}
 		if (unlikely(sleep(INTERVAL)))
-			ERR(ret);
+			ERR(nv_ret);
 	}
 	nv_cleanup();
 	return EXIT_SUCCESS;
@@ -124,5 +138,5 @@ nv_loop(void)
 int
 main(void)
 {
-	nv_loop();
+	nv_mainloop();
 }
