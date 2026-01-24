5593f3dbf13c7711307ae40e8c678d54aa851b69
James Tirta Halim <tirtajames45@gmail.com>
2026-01-24 17:56:45 +0700

    nvspeed.c: add TODO

--- nvspeed.c
+++ nvspeed.c
@@ -20,6 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE. */
 
+/* TODO: use graphs to generate fan speed */
 #include "config.h"
 
 #include NVML_HEADER
